bd3f53701f68179b9ba3d50cd4c05e07fafe4ad6
yyxi <178086254+yyxi@users.noreply.github.com>
2025-05-23 00:52:30 +0000

    feat: plugin-index-html

---
+++ packages/plugin-index-html/src/index.spec.ts
@@ -0,0 +1,76 @@
+import { describe, expect, it } from 'vitest'
+import { indexHTML } from '.'
+
+const content = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <!--
+
+        .                                 .o8       oooo
+      .o8                                "888        888
+    .o888oo oooo  oooo  ooo. .oo.  .oo.   888oooo.   888  oooo d8b
+      888    888   888   888P"Y88bP"Y88b  d88'  88b  888   888""8P
+      888    888   888   888   888   888  888   888  888   888
+      888 .  888   888   888   888   888  888   888  888   888    .o.
+      "888"   V88V"V8P' o888o o888o o888o  Y8bod8P' o888o d888b   Y8P
+
+    -->
+    <meta charset="utf-8" />
+    <meta name="referrer" content="no-referrer-when-downgrade" />
+    <link rel="preconnect" href="/" crossorigin />
+    <!--resource-hints-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="robots" content="max-snippet:-1, max-image-preview: large, max-video-preview: -1" />
+    <link type="text/plain" href="/humans.txt" rel="author" />
+    <!--head-tags-->
+    <script type="module" crossorigin src="/assets/js/main-BpwR5JBb.js"></script>
+    <link rel="modulepreload" crossorigin href="/assets/js/service-worker-3PDbR8De.js">
+    <link rel="stylesheet" crossorigin href="/assets/styles/main-C6nfZxQk.css">
+    <!-- -->
+  </head>
+
+  <body>
+    <!--body-tags-open-->
+    <div id="app">
+      <!--app-html-->
+    </div>
+    <div>
+    <!--
+        .                                 .o8       oooo
+      .o8                                "888        888
+    .o888oo oooo  oooo  ooo. .oo.  .oo.   888oooo.   888  oooo d8b
+      888    888   888   888P"Y88bP"Y88b  d88'  88b  888   888""8P
+      888    888   888   888   888   888  888   888  888   888
+      888 .  888   888   888   888   888  888   888  888   888    .o.
+      "888"   V88V"V8P' o888o o888o o888o  Y8bod8P' o888o d888b   Y8P
+
+    -->
+    </div>
+    <div id="app">
+      <!--app-html-->
+    <!--
+        .                                 .o8       oooo
+      .o8                                "888        888
+    .o888oo oooo  oooo  ooo. .oo.  .oo.   888oooo.   888  oooo d8b
+      888    888   888   888P"Y88bP"Y88b  d88'  88b  888   888""8P
+      888    888   888   888   888   888  888   888  888   888
+
+      888 .  888   888   888   888   888  888   888  888   888    .o.
+      "888"   V88V"V8P' o888o o888o o888o  Y8bod8P' o888o d888b   Y8P
+
+    -->
+    </div>
+    <!--initial-state-->
+    <!--body-tags-->
+  </body>
+</html>
+`
+
+describe('index-html', () => {
+  it('', async () => {
+    const handler = indexHTML().transformIndexHtml.handler
+
+    expect(await handler(content)).toMatchSnapshot()
+  })
+})
---
+++ packages/plugin-index-html/src/index.ts
@@ -0,0 +1,166 @@
+/* eslint-disable unicorn/consistent-function-scoping */
+import type { Root } from 'hast'
+import rehypeFormat from 'rehype-format'
+import rehypeParse from 'rehype-parse'
+import rehypeStringify from 'rehype-stringify'
+import { unified } from 'unified'
+import { visitParents } from 'unist-util-visit-parents'
+import type { Plugin } from 'vite'
+
+const voidElements = [
+  'area',
+  'base',
+  'br',
+  'col',
+  'embed',
+  'hr',
+  'img',
+  'input',
+  'link',
+  'meta',
+  'param',
+  'source',
+  'track',
+  'wbr',
+]
+
+export const indexHTML = () =>
+  ({
+    apply: 'serve',
+    enforce: 'post',
+    name: '@pointe/plugin-index-html',
+
+    transformIndexHtml: {
+      handler: async (source) => {
+        let commentContent = false
+
+        return String(
+          await unified()
+            .use(rehypeParse, { fragment: false })
+            .use(() => (tree: Root) => {
+              visitParents(
+                tree,
+                (node, parents) => {
+                  const parent = parents.at(-1)
+
+                  if (parent === undefined) {
+                    return
+                  }
+
+                  if (
+                    node.type === 'element' &&
+                    node.tagName === 'script' &&
+                    node.properties.type === 'module'
+                  ) {
+                    const index = parent.children.indexOf(node)
+
+                    if (index !== -1) {
+                      parent.children.splice(index, 1)
+                    }
+                  } else if (
+                    node.type === 'element' &&
+                    node.tagName === 'link' &&
+                    [node.properties.rel]
+                      .flat()
+                      .some((value) => value === 'modulepreload' || value === 'stylesheet')
+                  ) {
+                    const index = parent.children.indexOf(node)
+
+                    if (index !== -1) {
+                      parent.children.splice(index, 1)
+                    }
+                  } else if (node.type === 'comment') {
+                    const index = parent.children.indexOf(node)
+
+                    if (node.value.trim() === '' && index !== -1) {
+                      parent.children.splice(index, 1)
+                    }
+                  }
+                },
+                true,
+              )
+            })
+            .use(rehypeFormat, { blanks: ['head', 'body'], indent: '  ' })
+            .use(() => (tree: Root) => {
+              visitParents(
+                tree,
+                (node, parents) => {
+                  if (node.type === 'comment') {
+                    const parent = parents.at(-1)
+
+                    if (parent === undefined) {
+                      return
+                    }
+
+                    const index = parent.children.indexOf(node)
+
+                    if (index !== -1) {
+                      const level = (parents.length - 1) * 2
+                      const lines = node.value.split(/\r?\n/).map((value) => value.trim())
+
+                      parent.children.splice(index, 0, {
+                        type: 'text',
+                        value: level === 0 ? '\n' : `\n${' '.repeat(level)}`,
+                      })
+
+                      node.value = lines
+                        .map((value, index) => (index === 0 ? value : ' '.repeat(level) + value))
+                        .join('\n')
+                    }
+                  }
+                },
+                true,
+              )
+
+              visitParents(
+                tree,
+                (node, parents) => {
+                  const parent = parents.at(-1)
+
+                  if (parent === undefined) {
+                    return
+                  }
+
+                  if (
+                    node.type === 'element' &&
+                    node.tagName === 'div' &&
+                    node.children.length !== 0 &&
+                    node.children.every(
+                      (value) => value.type === 'comment' || value.type === 'text',
+                    ) &&
+                    node.children.at(-1)?.type === 'comment' &&
+                    !voidElements.includes(node.tagName)
+                  ) {
+                    const level = (parents.length - 1) * 2
+
+                    node.children.push({
+                      type: 'text',
+                      value: level === 0 ? '\n' : `\n${' '.repeat(level)}`,
+                    })
+                  }
+                },
+                true,
+              )
+            })
+            .use(rehypeStringify)
+            .process(source),
+        )
+          .split(/\r?\n/)
+          .filter((value) => {
+            const trimmed = value.trim()
+
+            if (trimmed.includes('<!--') && !commentContent) {
+              commentContent = true
+            }
+
+            if (trimmed.includes('-->') && commentContent) {
+              commentContent = false
+            }
+
+            return commentContent ? true : !/^\s*$/.test(value)
+          })
+          .join('\n')
+      },
+      order: 'post',
+    },
+  }) satisfies Plugin
